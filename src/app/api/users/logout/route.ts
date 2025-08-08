cbb930f24ca47c3684b57a1376eee93c11346866
Shravankale01 <165133204+Shravankale01@users.noreply.github.com>
2025-08-08 22:44:17 +0530

    Update route.ts

--- src/app/api/users/logout/route.ts
+++ src/app/api/users/logout/route.ts
@@ -1,4 +1,4 @@
-import { NextRequest, NextResponse } from "next/server";
+import { NextResponse } from "next/server";
 
 export async function POST() {
   const response = NextResponse.json({ message: "Logged out" });
@@ -6,3 +6,4 @@ export async function POST() {
   response.cookies.set("token", "", { httpOnly: true, expires: new Date(0) });
   return response;
 }
+
