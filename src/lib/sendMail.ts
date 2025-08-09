0b1065999f709dad5723c27792b0052cfd307ee9
Shravankale01 <shravankale491@gmail.com>
2025-08-09 14:59:40 +0530

    fix: sendMail

--- src/lib/sendMail.ts
+++ src/lib/sendMail.ts
@@ -1,18 +1,26 @@
 import nodemailer from 'nodemailer';
 
-export async function sendMail({ to, subject, html }) {
+interface SendMailParams {
+  to: string;
+  subject: string;
+  html: string;
+}
+
+export async function sendMail({ to, subject, html }: SendMailParams) {
   const transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
-      user: process.env.GMAIL_USER_EMAIL,     // your Gmail
-      pass: process.env.GMAIL_APP_PASSWORD    // your App Password
-    }
+      user: process.env.GMAIL_USER_EMAIL,      // your Gmail
+      pass: process.env.GMAIL_APP_PASSWORD,    // your App Password
+    },
   });
 
   await transporter.sendMail({
     from: process.env.GMAIL_USER_EMAIL,
     to,
     subject,
-    html
+    html,
   });
 }
+
+
